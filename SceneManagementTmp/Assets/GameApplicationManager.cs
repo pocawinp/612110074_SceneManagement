51b678e722f2374f7bf271c458d009cfa26c7822
DESKTOP-325D3AA\Slowanolf <pocawin_soiyana@cmu.ac.th>
2020-09-18 18:41:15 +0700

    Lab SceneManagment

---
+++ SceneManagementTmp/Assets/GameApplicationManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameApplicationManager : MonoBehaviour
+{
+    static public GameApplicationManager Instance
+    {
+ get
+        {
+             if (_instance == null)
+                 {
+                 _instance = GameObject.FindObjectOfType<GameApplicationManager>();
+                 GameObject container = new GameObject("GameApplicationManager");
+                 _instance = container.AddComponent<GameApplicationManager>();
+                 }
+             return _instance;
+             }
+ }
+ static protected GameApplicationManager _instance = null;
+
+                              
+
+        public string[] DIFFICULTY_LEVEL_NAMES = { "Easy", "Normal", "Hard", "Extreme" };
+
+        /////////// Get/Set property declaration ////////////
+        public bool IsOptionMenuActive
+        {
+        get { return _isOptionMenuActive; }
+        set { _isOptionMenuActive = value; }
+        }
+        protected bool _isOptionMenuActive = false;
+        //////////////////////////////
+
+        /////////// Shorter version of Get/Set property declaration ////////////
+        // The C# compiler will generate the same as the above for you, automatically.
+        public int DifficultyLevel { get; set; }
+        public bool MusicEnabled { get; set; }
+        public bool SFXEnabled { get; set; }
+        //////////////////////////////
+ }
+    
+
---
+++ SceneManagementTmp/Assets/MainMenuControlScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuControlScript : MonoBehaviour
+{
+  
+        
+        // Use this for initialization
+        void Start()
+        {
+        
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+             }
+
+        public void StartButtonClick(Button button)
+        {
+             SceneManager.LoadScene("SceneGameplay");
+             }
+
+        public void StartButtonClick2(Button button)
+        {
+            SceneManager.LoadScene("SceneGameplay2");
+        }
+
+        public void StartButtonClick3(Button button)
+        {
+            SceneManager.LoadScene("SceneGameplay3");
+        }
+
+
+
+    public void OptionsButtonClick(Button button)
+        {
+             if (!GameApplicationManager.Instance.IsOptionMenuActive)
+                 {
+                 SceneManager.LoadScene("SceneOptions", LoadSceneMode.Additive);
+                 GameApplicationManager.Instance.IsOptionMenuActive = true;
+                 }
+             }
+
+        public void ExitButtonClick(Button button)
+        {
+             Application.Quit();
+             }
+ 
+}
